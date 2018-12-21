3e9bff2ccc96e6fa26c11f2e8520b323f72956ef
cornellimartin <32455478+cornellimartin@users.noreply.github.com>
2018-12-21 17:15:36 -0300

    Comments added

--- JS/handleHome.js
+++ JS/handleHome.js
@@ -1,18 +1,18 @@
 function displayMenu() {
-    var show = document.getElementById("menuDiv");
-    if (show.classList.contains("hiddenMenu")){
+    var show = document.getElementById("menuDiv"); 
+    if (show.classList.contains("hiddenMenu")){ // If menu is hidden, display it.
         show.classList.replace("hiddenMenu", "displayedMenu");
-    }else{
+    }else{                                      // else, hide it
         show.classList.replace("displayedMenu", "hiddenMenu"); 
     }
-    event.stopPropagation();
+    event.stopPropagation(); // This avoid bubbling. If the menu is to be displayed, since onClick affects the body too, it will immmediately hide it back. With stopPropagation() we avoid this by handling onclick for the menu icon but stopping the bubbling up in the DOM till it reaches the body onclick event.
 }
 
+//The following block of code will consume a weather API, displaying only some of its elements, like city, country, temperature and condition
 var finalURL = "https://api.apixu.com/v1/forecast.json?key=a80c870049394166a9d33921181412&q=Montevideo";
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
         var text = JSON.parse(xhttp.responseText); 
         
         var locationString = text.location.name + ", " + text.location.country;
@@ -24,7 +24,7 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", finalURL, true);
 xhttp.send();
 
-
+//The following 4 functions handle hiding and displaying of divs to change pages.
 function goHome(){
     document.getElementById("home").style.display = "block";
     document.getElementById("gallery").style.display = "none";
@@ -53,6 +53,7 @@ function goContact(){
     document.getElementById("about").style.display = "none";
 }
 
+//Will hide the menu when clicking anywhere
 function hide(){
     var show = document.getElementById("menuDiv");
     if (show.classList.contains("displayedMenu")){
@@ -60,6 +61,7 @@ function hide(){
     }
 }
 
+//Will save name and or last name added to Contact form if Send button is clicked.
 function save_data() {
     var name = document.getElementById("getInfo")[0].value;
     var lastName = document.getElementById("getInfo")[1].value;
@@ -68,10 +70,12 @@ function save_data() {
     localStorage.setItem("lastName", lastName);
 }
 
+//Welcome message with name, last name or both if added and Send button clicked.
 function welcome(){
-    if (localStorage.getItem("name") != null || localStorage.getItem("lastName") != null){
+    if (localStorage.getItem("name") != "" || localStorage.getItem("lastName") != ""){
         var welcomeMessage = "Welcome back " + localStorage.getItem("name") + " " + localStorage.getItem("lastName");
 
         alert(welcomeMessage);
     }
 }
+
